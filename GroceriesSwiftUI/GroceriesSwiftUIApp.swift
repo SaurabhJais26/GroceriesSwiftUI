09129bc28e2f6dc30294ae7b3028567693fa3046
Saurabh Jaiswal <sjsaurabhjais@gmail.com>
2025-01-10 11:09:14 +0530

    Initial Commit

---
+++ GroceriesSwiftUI/GroceriesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceriesSwiftUIApp.swift
+//  GroceriesSwiftUI
+//
+//  Created by Saurabh Jaiswal on 10/01/25.
+//
+
+import SwiftUI
+
+@main
+struct GroceriesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
