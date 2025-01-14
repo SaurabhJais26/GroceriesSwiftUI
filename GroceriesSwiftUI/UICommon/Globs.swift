3af4a04d2d14fbc7069ac8295d5fec21d7130403
Saurabh Jaiswal <sjsaurabhjais@gmail.com>
2025-01-14 13:38:23 +0530

    Started the work of api integration.

---
+++ GroceriesSwiftUI/UICommon/Globs.swift
@@ -0,0 +1,13 @@
+//
+//  Globs.swift
+//  GroceriesSwiftUI
+//
+//  Created by Saurabh Jaiswal on 14/01/25.
+//
+
+import Foundation
+
+
+struct Globs {
+    static let appName = "Online Groceries"
+}
